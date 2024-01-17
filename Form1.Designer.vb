7b025a29c3140062a70fe629b4526e9f25d2093d
Academy-User <Academy-User@DESKTOP-1L3B0TP>
2024-01-17 13:23:20 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe Script", 16F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(150, -7)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(346, 53)
+        Label1.TabIndex = 0
+        Label1.Text = "BMI CALCULATOR"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(65, 111)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(153, 25)
+        Label2.TabIndex = 1
+        Label2.Text = "Enter Your Weight"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(65, 156)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(181, 25)
+        Label3.TabIndex = 1
+        Label3.Text = "Enter Your Height (m)"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(65, 203)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(83, 25)
+        Label4.TabIndex = 1
+        Label4.Text = "Your BMI"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(65, 259)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(91, 25)
+        Label5.TabIndex = 1
+        Label5.Text = "Comment"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(277, 108)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(150, 31)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(277, 150)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(150, 31)
+        TextBox2.TabIndex = 3
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(277, 203)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(150, 31)
+        TextBox3.TabIndex = 4
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(277, 256)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(150, 31)
+        TextBox4.TabIndex = 5
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(33, 343)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(161, 82)
+        Button1.TabIndex = 6
+        Button1.Text = "Calculator"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = My.Resources.Resources.How_to_Calculate_BMI____Body_Mass_Index___formula_in_kg_and_pounds
+        ClientSize = New Size(710, 450)
+        Controls.Add(Button1)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Comment"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button1 As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Declaration of varaibles
+        Dim height, weight, bmi As Double
+        weight = Val(TextBox1.Text)
+        height = Val(TextBox2.Text)
+
+        bmi = weight / (height) ^ 2 'BMI formula
+
+        TextBox3.Text = bmi
+
+        If bmi < 18 Then
+            TextBox4.Text = "You are underweight"
+        ElseIf 18 <= bmi And bmi < 26 Then
+            TextBox4.Text = "You are normal"
+        Else
+            TextBox4.Text = "You are overweight"
+        End If
+
+    End Sub
+End Class
